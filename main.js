96485a585f596b191d13dc83431708b10597d445
maosheng <zhangms@one-sports.com>
2021-10-19 14:21:52 +0800

    初始化

---
+++ main.js
@@ -0,0 +1,25 @@
+// 控制应用生命周期和创建原生浏览器窗口的模组
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  // 创建浏览器窗口
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  mainWindow.loadURL('http://localhost:3000/');
+}
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
